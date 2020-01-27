ac925f484c0ab907009af6eb0f4e0bd091c1d625
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-01-27 13:32:25 +0200

    init

--- SwiftUISpaceInDetails/ContentView.swift
+++ SwiftUISpaceInDetails/ContentView.swift
@@ -10,7 +10,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Spacer Explorer")
+                .font(.title)
+                .foregroundColor(.blue)
+            Image(systemName: "arrow.up.circle.fill")
+                .font(.title)
+                .foregroundColor(.blue)
+            Spacer()
+            HStack {
+                Image(systemName: "arrow.left.circle.fill")
+                    .font(.title)
+                    .foregroundColor(.blue)
+                Text("Spacer text")
+                Spacer(minLength: 20)
+                Text("Spacer in HStack")
+                Spacer(minLength: 20)
+                Image(systemName: "arrow.right.circle.fill")
+                    .font(.title)
+                    .foregroundColor(.blue)
+            }
+            Spacer()
+            Image(systemName: "arrow.down.circle.fill")
+                .font(.title)
+                .foregroundColor(.blue)
+        }
     }
 }
 
